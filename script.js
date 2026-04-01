537217ecf2442377fd88fe16aa6a717b86d55d06
Gaurab Dahal <dahalgaurab100@gmail.com>
2026-04-01 20:18:56 +0545

    Updating CNAME and script.js for correct custom domain and email submission

--- script.js
+++ script.js
@@ -209,16 +209,10 @@ async function sendEmail(e) {
 
     try {
         const formData = new FormData(form);
-        const object = Object.fromEntries(formData);
-        const json = JSON.stringify(object);
 
         const response = await fetch('https://api.web3forms.com/submit', {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: json
+            body: formData
         });
 
         const result = await response.json();
