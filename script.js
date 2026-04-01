794d9998c769b9e8fb02ce34db8d943af543df63
Gaurab Dahal <dahalgaurab100@gmail.com>
2026-04-01 15:24:57 +0545

    Fix Web3Forms submission on GitHub Pages by sending JSON data

--- script.js
+++ script.js
@@ -209,10 +209,16 @@ async function sendEmail(e) {
 
     try {
         const formData = new FormData(form);
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
 
         const response = await fetch('https://api.web3forms.com/submit', {
             method: 'POST',
-            body: formData
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
         });
 
         const result = await response.json();
